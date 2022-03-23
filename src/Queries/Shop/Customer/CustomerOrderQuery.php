0b046a48c1472e4afec0ad44d700a353df4092bf
Devansh <devansh.bawari419@webkul.com>
2022-03-23 17:50:01 +0530

    Fixed Order Authentication

--- src/Queries/Shop/Customer/CustomerOrderQuery.php
+++ src/Queries/Shop/Customer/CustomerOrderQuery.php
@@ -2,13 +2,14 @@
 
 namespace Webkul\GraphQLAPI\Queries\Shop\Customer;
 
-use Webkul\Customer\Http\Controllers\Controller;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Webkul\Customer\Http\Controllers\Controller;
+use Webkul\Sales\Repositories\OrderRepository;
 
 class CustomerOrderQuery extends Controller
 {
     /**
-     * Contains current guard
+     * Contains current guard.
      *
      * @var array
      */
@@ -24,71 +25,88 @@ class CustomerOrderQuery extends Controller
         $this->guard = 'api';
 
         auth()->setDefaultDriver($this->guard);
-        
+
         $this->middleware('auth:' . $this->guard);
     }
 
     /**
-     * Returns loggedin customer's reviews data.
+     * Returns loggedin customer's orders data.
      *
-     * @return \Illuminate\Http\Response
+     * @param  mixed  $query
+     * @param  mixed  $input
+     * @param  mixed  $test
+     * @return mixed
      */
     public function orders($query, $input, $test)
     {
         $params = $input;
-        
-        if ( bagisto_graphql()->guard($this->guard)->check() ) {
-            $params['customer_id'] = bagisto_graphql()->guard($this->guard)->user()->id;
-        }
-        
-        $qb = $query->distinct()
-            ->addSelect('orders.*');
 
-        if ( isset($params['id']) && $params['id']) {
-            $qb->where('orders.id', $params['id']);
+        if (! bagisto_graphql()->guard($this->guard)->check()) {
+            return null;
         }
 
-        if ( isset($params['incrementId']) && $params['incrementId']) {
+        $qb = app(OrderRepository::class)->query()->distinct()->addSelect('orders.*');
+
+        $params['customer_id'] = bagisto_graphql()->guard($this->guard)->user()->id;
+
+        if (isset($params['input']['id']) && $params['input']['id']) {
+            $qb->where('orders.id', $params['input']['id']);
+        }
+
+        if (isset($params['incrementId']) && $params['incrementId']) {
             $qb->where('orders.incrementId', $params['incrementId']);
         }
 
-        if ( isset($params['baseSubTotal']) && $params['baseSubTotal']) {
+        if (isset($params['baseSubTotal']) && $params['baseSubTotal']) {
             $qb->where('orders.baseSubTotal', $params['baseSubTotal']);
         }
 
-        if ( isset($params['baseGrandTotal']) && $params['baseGrandTotal']) {
+        if (isset($params['baseGrandTotal']) && $params['baseGrandTotal']) {
             $qb->where('orders.baseGrandTotal', $params['baseGrandTotal']);
         }
 
-        if ( isset($params['baseGrandTotal']) && $params['baseGrandTotal']) {
+        if (isset($params['baseGrandTotal']) && $params['baseGrandTotal']) {
             $qb->where('orders.baseGrandTotal', $params['baseGrandTotal']);
         }
 
-        if ( isset($params['orderDate']) && $params['orderDate']) {
+        if (isset($params['orderDate']) && $params['orderDate']) {
             $qb->where('orders.orderDate', 'like', '%' . urldecode($params['orderDate']) . '%');
         }
-        
-        if ( isset($params['customer_id']) && $params['customer_id']) {
+
+        if (isset($params['customer_id']) && $params['customer_id']) {
             $qb->where('orders.customer_id', $params['customer_id']);
         }
 
-        if ( isset($params['status']) && $params['status']) {
+        if (isset($params['status']) && $params['status']) {
             $qb->where('orders.status', $params['status']);
         }
 
         return $qb->orderBy('orders.id', 'desc');
     }
 
+    /**
+     * Get order payment additional data.
+     *
+     * @param  mixed  $rootValue
+     * @param  array  $args
+     * @param  GraphQLContext  $context
+     * @return [type]
+     */
     public function getOrderPaymentAdditional($rootValue, array $args, GraphQLContext $context)
     {
         return \Webkul\Payment\Payment::getAdditionalDetails($rootValue->method);
     }
 
+    /**
+     * Get order payment title.
+     *
+     * @param  mixed  $rootValue
+     * @param  array  $args
+     * @param  GraphQLContext  $context
+     * @return mixed
+     */
     public function getOrderPaymentTitle($rootValue, array $args, GraphQLContext $context)
     {
         return core()->getConfigData('sales.paymentmethods.' . $rootValue->method . '.title');
     }
 }
-
-
-
