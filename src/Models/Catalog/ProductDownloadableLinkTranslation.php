2fc2ccfdf0edb9c4bb669f5bdbaafe616199bb9a
rachana chaturvedi <rachana.chaturvedi809@webkul.in>
2023-11-07 16:24:55 +0530

    updated product api

---
+++ src/Models/Catalog/ProductDownloadableLinkTranslation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Webkul\GraphQLAPI\Models\Catalog;
+
+use Illuminate\Support\Facades\Storage;
+use Webkul\Product\Models\ProductDownloadableLinkTranslation as BaseModel;
+
+class ProductDownloadableLinkTranslation extends BaseModel
+{
+}
