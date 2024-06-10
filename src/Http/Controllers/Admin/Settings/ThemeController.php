e133bd295b9562f11e9cc9036bd75d86f531d621
Vivek Sharma <viveksh047@webkul.in>
2024-06-10 16:10:54 +0530

    [Fixed: Theme multi-locale issues.]

---
+++ src/Http/Controllers/Admin/Settings/ThemeController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Webkul\Admin\Http\Controllers\Settings;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Webkul\Admin\Http\Controllers\Controller;
+use Webkul\Shop\Repositories\ThemeCustomizationRepository;
+use Webkul\Admin\DataGrids\Theme\ThemeDatagrid;
+
+class ThemeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(public ThemeCustomizationRepository $themeCustomizationRepository)
+    {
+    }
+
+    /**
+     * Display a listing resource for the available tax rates.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            return app(ThemeDatagrid::class)->toJson();
+        }
+
+        return view('admin::settings.themes.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @return \Illuminate\Http\JsonResponse|string
+     */
+    public function store()
+    {
+        if (request()->has('id')) {
+            $this->validate(request(), [
+                core()->getRequestedLocaleCode().'.options.*.image' => 'image|extensions:jpeg,jpg,png,svg,webp',
+            ]);
+
+            $theme = $this->themeCustomizationRepository->find(request()->input('id'));
+
+            return $this->themeCustomizationRepository->uploadImage(request()->all(), $theme);
+        }
+
+        $validated = $this->validate(request(), [
+            'name'       => 'required',
+            'sort_order' => 'required|numeric',
+            'type'       => 'required|in:product_carousel,category_carousel,static_content,image_carousel,footer_links,services_content',
+            'channel_id' => 'required|in:'.implode(',', (core()->getAllChannels()->pluck('id')->toArray())),
+        ]);
+
+        Event::dispatch('theme_customization.create.before');
+
+        $theme = $this->themeCustomizationRepository->create($validated);
+
+        Event::dispatch('theme_customization.create.after', $theme);
+
+        return new JsonResponse([
+            'redirect_url' => route('admin.settings.themes.edit', $theme->id),
+        ]);
+    }
+
+    /**
+     * Edit the theme
+     *
+     * @param integer $id
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $theme = $this->themeCustomizationRepository->find($id);
+
+        return view('admin::settings.themes.edit', compact('theme'));
+    }
+
+    /**
+     * Update the specified resource
+     *
+     * @param integer $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update($id)
+    {
+        $this->validate(request(), [
+            'name'       => 'required',
+            'sort_order' => 'required|numeric',
+            'type'       => 'required|in:product_carousel,category_carousel,static_content,image_carousel,footer_links,services_content',
+            'channel_id' => 'required|in:'.implode(',', (core()->getAllChannels()->pluck('id')->toArray())),
+        ]);
+
+        $locale = request('locale');
+
+        $data = request()->only(
+            'locale',
+            'type',
+            'name',
+            'sort_order',
+            'channel_id',
+            'status',
+            $locale
+        );
+
+        Event::dispatch('theme_customization.update.before', $id);
+
+        $data['status'] = request()->input('status') == 'on';
+
+        $theme = $this->themeCustomizationRepository->update($data, $id);
+
+        Event::dispatch('theme_customization.update.after', $theme);
+
+        session()->flash('success', trans('admin::app.settings.themes.update-success'));
+
+        return redirect()->route('admin.settings.themes.index');
+    }
+
+    /**
+     * Delete a specified theme.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        Event::dispatch('theme_customization.delete.before', $id);
+
+        $this->themeCustomizationRepository->delete($id);
+
+        Storage::deleteDirectory('theme/'.$id);
+
+        Event::dispatch('theme_customization.delete.after', $id);
+
+        return new JsonResponse([
+            'message' => trans('admin::app.settings.themes.delete-success'),
+        ], 200);
+    }
+}
