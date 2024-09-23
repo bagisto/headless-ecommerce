542ce355822d2b27b0a42b573ee798808550a35c
mayank-bagisto <mayank.kumar190@webkul.in>
2024-09-23 12:48:49 +0530

    Skip CMS Pages Without Translations

--- src/Queries/Admin/Cms/FilterCmsPage.php
+++ src/Queries/Admin/Cms/FilterCmsPage.php
@@ -15,13 +15,15 @@ class FilterCmsPage extends BaseFilter
     {
         $params = Arr::except($input, ['page_title', 'url_key']);
 
-        if (isset($input['page_title'])) {
-            $query->whereHas('translations', fn ($q) => $q->where('page_title', $input['page_title']));
-        }
+        $query->whereHas('translations', function ($q) use ($input) {
+            if (isset($input['page_title'])) {
+                $q->where('page_title', $input['page_title']);
+            }
 
-        if (isset($input['url_key'])) {
-            $query->whereHas('translations', fn ($q) => $q->where('url_key', $input['url_key']));
-        }
+            if (isset($input['url_key'])) {
+                $q->where('url_key', $input['url_key']);
+            }
+        });
 
         return $query->where($params);
     }
