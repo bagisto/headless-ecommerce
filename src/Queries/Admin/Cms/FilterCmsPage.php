ef50a8bd19242514903fa3c1415128cd41cac9d3
mayank-bagisto <mayank.kumar190@webkul.in>
2024-09-23 11:00:09 +0530

    CMS Page Filter Issue Fixed

--- src/Queries/Admin/Cms/FilterCmsPage.php
+++ src/Queries/Admin/Cms/FilterCmsPage.php
@@ -2,6 +2,7 @@
 
 namespace Webkul\GraphQLAPI\Queries\Admin\Cms;
 
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Builder;
 use Webkul\GraphQLAPI\Queries\BaseFilter;
 
@@ -12,22 +13,16 @@ class FilterCmsPage extends BaseFilter
      */
     public function __invoke(Builder $query, array $input): Builder
     {
-        if (isset($input['page_title'])) {
-            $query = $query->whereHas('page_title', function ($q) use ($input) {
-                $q->where('page_title', $input['page_title']);
-            });
+        $params = Arr::except($input, ['page_title', 'url_key']);
 
-            unset($input['page_title']);
+        if (isset($input['page_title'])) {
+            $query->whereHas('translations', fn ($q) => $q->where('page_title', $input['page_title']));
         }
 
         if (isset($input['url_key'])) {
-            $query = $query->whereHas('url_key', function ($q) use ($input) {
-                $q->where('url_key', $input['url_key']);
-            });
-
-            unset($input['url_key']);
+            $query->whereHas('translations', fn ($q) => $q->where('url_key', $input['url_key']));
         }
 
-        return $query->where($input);
+        return $query->where($params);
     }
 }
