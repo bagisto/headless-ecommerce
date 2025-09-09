78505a5163513bc24dbc4f01701f99ff46dfa47b
mayank-bagisto <mayank.kumar190@webkul.in>
2025-09-09 15:57:20 +0530

    fix: update authorization key and enhance security checks in GraphQL API

--- src/Queries/Admin/Configuration/FilterConfiguration.php
+++ src/Queries/Admin/Configuration/FilterConfiguration.php
@@ -4,6 +4,9 @@ namespace Webkul\GraphQLAPI\Queries\Admin\Configuration;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
+use Nuwave\Lighthouse\Exceptions\AuthenticationException;
 use Webkul\GraphQLAPI\Queries\BaseFilter;
 
 class FilterConfiguration extends BaseFilter
@@ -13,6 +16,8 @@ class FilterConfiguration extends BaseFilter
      */
     public function __invoke(Builder $query, array $input): Builder
     {
+        $this->ensureFrontendRequestWasSecured();
+
         $query = $this->applyFilter($query, Arr::only($input, ['channel_code', 'locale_code']));
 
         return $this->applyLikeFilter($query, Arr::only($input, ['code']));
@@ -23,6 +28,27 @@ class FilterConfiguration extends BaseFilter
      */
     public function get(mixed $rootvalue, array $args): ?string
     {
+        $this->ensureFrontendRequestWasSecured();
+
         return core()->getConfigData($args['code']);
+    }
+
+    /**
+     * Ensure that the frontend request was secured with the correct API key.
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function ensureFrontendRequestWasSecured(): void
+    {
+        $headerKey = Request::header('x-app-secret-key');
+
+        $envKey = Str::of(env('APP_SECRET_KEY'))->replace('base64:', '')->toString();
+
+        if (
+            is_null($headerKey)
+            || $headerKey !== $envKey
+        ) {
+            throw new AuthenticationException('Invalid or missing app secret key.');
+        }
     }
 }
